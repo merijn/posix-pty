501bf6be8a21c884e45a5bfbecbcb4d053742bef
Vladimir Kirillov <proger@wilab.org.ua>
2014-10-22 13:29:39 +0300

    update ptywrap to not do buffering and wait for the child process

--- ptywrap.hs
+++ ptywrap.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE LambdaCase #-}
 
-import System.Environment (getArgs)
-import Control.Monad (forever)
+import Control.Monad (forever, (>=>))
 import qualified Data.ByteString.Char8 as C8
 
+import System.Environment (getArgs)
+import System.IO
+import System.IO.Error
+import System.Exit (exitWith)
+import System.Process (waitForProcess)
 import System.Posix.Pty
 
 main :: IO ()
 main = do
   (command:args) <- getArgs >>= \case [] -> error "usage: ptywrap [command to run]"
                                       x -> return x
-  (pty, _) <- spawnWithPty Nothing False command args (80, 25)
-  forever $ do
-    r <- readPty pty
-    C8.putStr r
+  hSetBuffering stdout NoBuffering
+  (pty, proc) <- spawnWithPty Nothing False command args (80, 25)
+  forever $ (tryIOError . readPty) pty >>= \
+    case Left e | isEOFError e -> waitForProcess proc >>= exitWith
+         Left e -> ioError e
+         Right s -> C8.putStr s
